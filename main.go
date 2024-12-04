22db88a6302299bb3dd33d31512d0e91c6a2514f
Kevin <kumasambakyg@gmail.com>
2024-12-04 13:52:34 +0200

    using interfaces and embedded interfaces for todo and note structs

--- main.go
+++ main.go
@@ -5,12 +5,33 @@ import (
 	"fmt"
 	"os"
 	"strings"
-
+	"example.kyg/notes/todo"
 	"example.kyg/notes/note"
 )
 
+// naming convention if your interface only has one method
+// checks struct to see if it has a Save method with the right arguments and return typess
+type saver interface {
+	Save() error
+} 
+type displayer interface {
+	Display()
+}
+
+// a combined interface
+// type outputtable interface {
+// 	Save() error
+// 	Display()
+// }
+// or even better an embedded interface
+type outputtable interface {
+	saver
+	displayer
+}
 func main() {
 	
+	// NOTE SECTION
+
 	title, content, err := getNote()
 	if err != nil {
 		fmt.Print("\n", err, "\n")
@@ -23,15 +44,52 @@ func main() {
 		return 
 	}
 
-	userNote.Display()
-	err = userNote.Save()
+	err = outputData(userNote)
 
 	if err != nil {
-		fmt.Println("Saving failed")
+		fmt.Println("Saving note failed")
 		return
 	}
-	fmt.Println("Saving succeeded")
+	fmt.Println("Saving note succeeded")
+
+	// TODO SECTION
+
+	activity, err := getTodo()
+	if err != nil {
+		fmt.Print("\n", err, "\n")
+		return 
+	}
+
+	userTodo, err := todo.New(activity)
+	if err != nil {
+		fmt.Print("\n", err, "\n")
+		return 
+	}
+	err = outputData(userTodo)
+
+	if err != nil {
+		fmt.Println("Saving todo failed")
+		return
+	}
+	fmt.Println("Saving todo succeeded")
+	
 }
+
+func saveData(data saver) error {
+	err := data.Save()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func outputData(data outputtable) error {
+	data.Display()
+	return saveData(data)
+}
+
 func getNote() (string, string, error) {
 	title, errTitle := getInput("Note title: ")
 	content, errContent := getInput("Note content: ")
@@ -45,6 +103,17 @@ func getNote() (string, string, error) {
 	}
 
 	return title, content, nil
+}
+
+func getTodo() (string, error) {
+	content, err := getInput("Add a to-do: ")
+
+	if err != nil {
+		fmt.Print(err)
+		return "", err
+	}
+
+	return content, nil
 }
 
 // handling long user input text
